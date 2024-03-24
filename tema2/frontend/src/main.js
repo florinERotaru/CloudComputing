44f9b27a7744ae60f12cf5b110681f1a8c1c7aaa
florin rotaru <florineugen271@gmail.com>
2024-03-24 19:17:48 +0200

    tema2

--- tema2/frontend/src/main.js
+++ tema2/frontend/src/main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import AliceCarousel from 'react-alice-carousel';
 import './styles/slides.css';
@@ -8,6 +8,8 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 const Main = () => {
   const [movies, setMovies] = useState([]);
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [searchInput, setSearchInput] = useState('');
+  const carousel = useRef(null); // Add this line to create the carousel ref
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -22,25 +24,54 @@ const Main = () => {
     fetchMovies();
   }, []);
 
+  const handleSearch = () => {
+    const trimmedSearchInput = searchInput.trim().toUpperCase();
+    const foundIndex = movies.findIndex(movie =>
+      movie.title.toUpperCase().includes(trimmedSearchInput)
+    );
+
+    if (foundIndex !== -1) {
+      if (foundIndex < currentSlide || foundIndex >= currentSlide + 4) {
+        const scrollDistance = foundIndex - currentSlide;
+        carousel.current.slideTo(currentSlide + scrollDistance); // Access the carousel using the ref
+        setCurrentSlide(foundIndex);
+      } else {
+        setCurrentSlide(foundIndex);
+      }
+    } else {
+      console.log('Movie not found');
+    }
+  };
+
   const items = movies.map((movie, index) => (
-    <div key={index} className="slideContainer" onClick={() => setCurrentSlide(index)}>
-      <div className={index === currentSlide ? 'focusedThumbnail' : 'shadowedThumbnail'}>
+    <div
+      key={index}
+      className={index === currentSlide ? 'slideContainer focusedThumbnail' : 'slideContainer shadowedThumbnail'}
+      onClick={() => handleSlideClick(index)}
+    >
+      <div>
         <img src={movie.thumbnail} alt={movie.title} />
         <p>{movie.title}</p>
       </div>
       {index === currentSlide && (
         <div className="slideInfo">
           <p><strong>Genres:</strong> {movie.genres.join(', ')}</p>
-          <p>Actors: {movie.cast.join(', ')}</p>
-          <p>Year: {movie.year}</p>
-          <p>Plot: {movie.extract}</p>
+          <p><strong>Cast:</strong>: {movie.cast.join(', ')}</p>
+          <p><strong>Year:</strong>: {movie.year}</p>
+          <p><strong>Plot:</strong>: {movie.extract}</p>
         </div>
       )}
     </div>
   ));
+
+  const handleSlideClick = (index) => {
+    setCurrentSlide(index);
+    if (carousel.current) {
+      carousel.current.slideTo(index);
+    }
+  };
   
   const onSlideChanged = (e) => {
-    console.debug(`onSlideChange => Item's position before a change: ${e.item}. Event:`, e);
     setCurrentSlide(e.item);
     console.log(movies[e.item].title);
   };
@@ -55,6 +86,13 @@ const Main = () => {
     <div>
       <div align='center'>
         <h2 className="text">Featured Movies</h2>
+        <input 
+          className='searchBar'
+          onChange={event => setSearchInput(event.target.value)}
+          value={searchInput}
+          placeholder="Enter The Title Of Movie"
+        />
+        <button onClick={handleSearch}>Search</button>
       </div>
       <div className="slider">
         <AliceCarousel
@@ -63,6 +101,7 @@ const Main = () => {
           responsive={responsive}
           disableDotsControls={true}
           onSlideChanged={onSlideChanged}
+          ref={carousel} // Assign the ref to the AliceCarousel component
         />
       </div>
     </div>
